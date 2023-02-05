6cb475299a4a2cccca114203e79080ff4d0fac9e
Davi <120661012+Pancoty@users.noreply.github.com>
2023-02-05 13:02:48 -0300

    Mudando o local dos arquivos.
    Erro de imagem corrigido.

--- arquivos.js
+++ arquivos.js
@@ -12,10 +12,10 @@ let pontoS;
 
 function preload(){//CARREGA OS ARQUIVOS
   estrada = loadImage("images/estrada.png");//CARREGA A IMAGEM DA ESTRADA
-  A = loadImage("images/ator.png");//CARREGA A IMAGEM DO ATOR
-  C1 = loadImage("images/carro_1.png");//CARREGA A IMAGEM DO CARRO 1
-  C2 = loadImage("images/carro_3.png");//CARREGA A IMAGEM DO CARRO 2
-  C3 = loadImage("images/carro_2.png");//CARREGA A IMAGEM DO CARRO 3
+  A = loadImage("images/ator-1.png");//CARREGA A IMAGEM DO ATOR
+  C1 = loadImage("images/carro-1.png");//CARREGA A IMAGEM DO CARRO 1
+  C2 = loadImage("images/carro-3.png");//CARREGA A IMAGEM DO CARRO 2
+  C3 = loadImage("images/carro-2.png");//CARREGA A IMAGEM DO CARRO 3
   C = [C1, C2, C3, C1, C2, C3];//LISTA DAS IMAGENS
 
   trilhaS = loadSound("sounds/trilha.mp3")//CARREGA O SOM DA TRILHA
